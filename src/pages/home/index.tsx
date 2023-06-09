d68835816daee8c5cc83ba53abe15ecb97b0f363
Paulo Henrique <paulo.hsilvavieira@gmail.com>
2023-06-09 15:30:08 -0300

    feat: create home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,11 @@
 /* eslint-disable @next/next/no-title-in-document-head */
-import { H1 } from '@/atoms/heading';
 
+import { ButtonLink } from '@/atoms/button';
+import FlexBox from '@/atoms/flex';
+import { H3 } from '@/atoms/heading';
+import { CardContainer } from '@/molecules/card';
+import { CardWallet } from '@/molecules/card-wallet';
+import { Container } from '@/molecules/container';
 import Head from 'next/head';
 
 export default function Home() {
@@ -9,8 +14,47 @@ export default function Home() {
       <Head>
         <title>Home</title>
       </Head>
-
-      <H1>Hello World!</H1>
+      <Container>
+        <FlexBox justify="center" style={{ margin: '20px 0px' }}>
+          <H3>Wallets</H3>
+        </FlexBox>
+        <FlexBox gap="10px" justify="space-between">
+          <CardWallet
+            walletName="Main Portifolio"
+            amountCrypto="BTC 100.00000000"
+            amountFiat="R$ 342,00"
+            changePercent={'92%'}
+            typeChange={'down'}
+          />
+          <CardWallet
+            walletName="Money 2"
+            amountCrypto="BTC 0.81271366"
+            amountFiat="R$ 30.000,00"
+            changePercent={'0%'}
+            typeChange={'down'}
+          />
+          <CardWallet
+            walletName="Money"
+            amountCrypto="BTC 100"
+            amountFiat="R$ 30.000,00"
+            changePercent={'12%'}
+            typeChange={'up'}
+          />
+          <CardWallet
+            walletName="Money"
+            amountCrypto="BTC 100"
+            amountFiat="R$ 30.000,00"
+            changePercent={'12%'}
+            typeChange={'up'}
+          />
+        </FlexBox>
+        <FlexBox justify="center" style={{ margin: '20px 0px' }}>
+          <ButtonLink typeButton="primary" sizeButton="md">
+            See All Wallets
+          </ButtonLink>
+        </FlexBox>
+        <CardContainer width="100%"></CardContainer>
+      </Container>
     </>
   );
 }

13ab63f39f16b2aae9902db8627a845cc1927284
Paulo Henrique <paulo.hsilvavieira@gmail.com>
2023-06-09 15:39:39 -0300

    feat: change color heading text

--- src/atoms/heading/index.ts
+++ src/atoms/heading/index.ts
@@ -3,16 +3,19 @@ import styled from 'styled-components';
 export const H1 = styled.h1`
   font-size: 2.5rem;
   font-weight: bold;
+  color: #333333;
 `;
 
 export const H2 = styled.h2`
   font-size: 2rem;
   font-weight: bold;
+  color: #333333;
 `;
 
 export const H3 = styled.h3`
   font-size: 1.8rem;
   font-weight: bold;
+  color: #333333;
 `;
 
 export const H4 = styled.h4`
@@ -23,9 +26,11 @@ export const H4 = styled.h4`
 export const H5 = styled.h5`
   font-size: 1.2rem;
   font-weight: bold;
+  color: #333333;
 `;
 
 export const H6 = styled.h6`
   font-size: 1rem;
   font-weight: bold;
+  color: #333333;
 `;
